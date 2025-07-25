284d3702c6e3f491a3c91bcbc3caa7552f139066
JasperXzy <jasper.zhengyi.xu@gmail.com>
2025-07-25 21:35:53 +0800

    优化launch文件

--- launch/racing_segmentation.launch.py
+++ launch/racing_segmentation.launch.py
@@ -1,12 +1,12 @@
 # racing_segmentation/launch/racing_segmentation.launch.py
 
 import os
-import subprocess
-from ament_index_python.packages import get_package_prefix
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    launch_actions = []
+
     segmentation_node = Node(
         package='racing_segmentation',
         executable='racing_segmentation',
@@ -17,24 +17,28 @@ def generate_launch_description():
         remappings=[
         ]
     )
+    launch_actions.append(segmentation_node)
 
-    websocket_node = Node(
-        package='websocket',
-        executable='websocket',
-        name='websocket',
-        output='screen',
-        parameters=[
-            {"image_topic": "/image"},
-            {"image_type": "mjpeg"},
-            {"only_show_image": False},
-            {"output_fps": 0},
-            {"smart_topic": "/racing_segmentation"},
-            {"channel": 0}
-        ],
-        arguments=['--ros-args', '--log-level', 'info']
-    )
+    if os.getenv('WEB_SHOW') == 'True':
+        print("\n'WEB_SHOW' is set to True, launching websocket_node for segmentation.\n")
+        
+        websocket_node = Node(
+            package='websocket',
+            executable='websocket',
+            name='websocket',
+            output='screen',
+            parameters=[
+                {"image_topic": "/image"},
+                {"image_type": "mjpeg"},
+                {"only_show_image": False},
+                {"output_fps": 0},
+                {"smart_topic": "/racing_segmentation"}, 
+                {"channel": 0}
+            ],
+            arguments=['--ros-args', '--log-level', 'info']
+        )
+        launch_actions.append(websocket_node)
+    else:
+        print("\n'WEB_SHOW' is not set to True, skipping websocket_node.\n")
 
-    return LaunchDescription([
-        segmentation_node,
-        websocket_node
-    ])
+    return LaunchDescription(launch_actions)
