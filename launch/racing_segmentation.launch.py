93316fef01e0dcf8ec7d3417a17339c57641e937
JasperXzy <jasper.zhengyi.xu@gmail.com>
2025-07-25 20:36:25 +0800

    修复了一些bug

--- launch/racing_segmentation.launch.py
+++ launch/racing_segmentation.launch.py
@@ -1,40 +1,64 @@
-# racing_segmentation/launch/racing_segmentation.launch.py
-
 import os
-import subprocess
-from ament_index_python.packages import get_package_prefix
+
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import TextSubstitution
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python import get_package_share_directory
+
 
 def generate_launch_description():
-    segmentation_node = Node(
-        package='racing_segmentation',
-        executable='racing_segmentation',
-        name='racing_segmentation',
-        output='screen',
-        parameters=[
-        ],
-        remappings=[
-        ]
+    image_width_launch_arg = DeclareLaunchArgument(
+        "dnn_sample_image_width", default_value=TextSubstitution(text="640")
+    )
+    image_height_launch_arg = DeclareLaunchArgument(
+        "dnn_sample_image_height", default_value=TextSubstitution(text="480")
     )
 
-    websocket_node = Node(
-        package='websocket',
-        executable='websocket',
-        name='websocket',
+    web_show = os.getenv('WEB_SHOW')
+    print("web_show is ", web_show)
+
+    jpeg_codec_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('hobot_codec'),
+                'launch/hobot_codec_encode.launch.py')),
+        launch_arguments={
+            'codec_in_mode': 'shared_mem',
+            'codec_out_mode': 'ros',
+            'codec_sub_topic': '/hbmem_img',
+            'codec_pub_topic': '/image_jpeg',
+        }.items()
+    )
+
+    web_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('websocket'),
+                'launch/websocket.launch.py')),
+        launch_arguments={
+            'websocket_image_topic': '/image_jpeg',
+            'websocket_smart_topic': '/racing_segmentation'
+        }.items()
+    )
+
+    racing_segmentation_node = Node(
+        package='racing_segmentation',
+        executable='racing_segmentation',
         output='screen',
-        parameters=[
-            {"image_topic": "/image"},
-            {"image_type": "mjpeg"},
-            {"only_show_image": False},
-            {"output_fps": 15},
-            {"smart_topic": "/racing_segmentation"},
-            {"channel": 0}
-        ],
         arguments=['--ros-args', '--log-level', 'info']
     )
 
-    return LaunchDescription([
-        segmentation_node,
-        websocket_node
-    ])
+    if web_show == "TRUE":
+        return LaunchDescription([
+            racing_segmentation_node,
+            jpeg_codec_node,
+            web_node
+        ])
+    else:
+        return LaunchDescription([
+            racing_segmentation_node
+        ])
